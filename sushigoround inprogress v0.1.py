177cadde588ad5761eeb48c6d39b86127c12b55f
Evgeny <45047292+evigit@users.noreply.github.com>
2018-12-07 01:07:41 +0300

    Update sushigoround inprogress v0.1.py
    done: 
    - controlling of mouse movement
    - startServing()  - main gameplay function
    - clearing the Plates - it clicks the mouse on the six places where finished plates will be flashing. This function does
        not check for flashing plates, but simply clicks on all six places """
    - getting the Orders from Customers - def getOrders():
    and def getOrdersDifference()

--- sushigoround inprogress v0.1.py
+++ sushigoround inprogress v0.1.py
@@ -144,5 +144,182 @@ def setupCoordinates():
 
     LEVEL = 1
 
+    
+def navigateStartGameMenu():
+    '''Performs the clicks to navigate form the start screen to the beginning of the first level'''
+    # Click on everything needed to get past the menus at the start of the game.
 
-#pyautogui.locateOnScreen()
+    # click on Play
+    logging.debug('Looking for Play button...')
+    while True: # loop because it could be the blue or pink Play button displayed at the moment.
+        pos = pyautogui.locateCenterOnScreen(imPath('play_button.png'), region=GAME_REGION)  # pos stands for position
+        if pos is not None:
+            break
+    pyautogui.click(pos, duration=0.25)
+    logging.debug('Clicked on Play button.')
+
+    # click on Continue
+    pos = pyautogui.locateCenterOnScreen(imPath('continue_button.png'), region=GAME_REGION)
+    pyautogui.click(pos, duration=0.25)
+    logging.debug('Clicked on Continue button.')
+
+    # click on Skip
+    logging.debug('Looking for Skip button...')
+    while True:  # loop because it could be the yellow or red Skip button displayed at the moment.
+        pos = pyautogui.locateCenterOnScreen(imPath('skip_button.png'), region=GAME_REGION)
+        if pos is not None:
+            break
+    pyautogui.click(pos, duration=0.25)
+    logging.debug('Clicked on Skip Button.')
+
+    # click on Continue
+    pos = pyautogui.locateCenterOnScreen(imPath('continue_button.png'), region=GAME_REGION)
+    pyautogui.click(pos, duration=0.25)
+    logging.debug('Clicked on Continue button.')
+
+
+def startServing():
+    """main game playing function. handles all game play: identifying orders, making orders, buying ingredients, etc."""
+    global LAST_GAME_OVER_CHECK, INVENTORY, ORDERING_COMPLETE, LEVEL
+
+    # Reset all game state variables.
+    oldOrders = {}
+    backOrders = {}
+    remakeOrders = {}
+    remakeItems = {}
+    LAST_GAME_OVER_CHECK = time.time()
+    ORDERING_COMPLETE = {SHRIMP: None, RICE: None, NORI: None,
+                         ROE: None, SALMON: None, UNAGI: None}
+
+
+# The next part scans the region where customers orders are displayed:
+
+    while True:
+        # Check for orders, see which are new and which are gone since last time.
+        currentOrders = getOrders()
+        added, removed = getOrdersDifference(currentsOrders, oldOrders)
+        if added != {}:
+            logging.debug('New orders: %s' % (list(added.values())))
+            for k in added:
+                remakeTimes[k] = time.time() + TIME_TO_REMAKE
+        if removed != {}:
+            logging.debug('Removed orders: %s' % (list(removed.values())))
+            for k in removed:
+                del remakeTimes[k]
+
+        #Check if the remake times have past, and add those to the remakeOrders, dictionary.
+        for k, remakeTime in copy.copy(remakeTimes).items():
+            if time.time() > remakeTime:
+                remakeTimes[k] = time.time() + TIME_TO_REMAKE # reset remake time
+                remakeOrders[k] = currentsOrders[k]
+                logging.debug('%s added to remake orders.' % (currentOrders[k]))
+
+        #Attempt to make the order.
+        for pos, order in added.items():
+            result = makeOrder(order)
+            if result is not None:
+                orderIngredient(result)
+                backOrders[pos] = order
+                logging.debug('Ingredients for %s not available. Putting on back order.' % (order))
+
+        # Clear any finished plates
+        if random.randint(1,10) == 1 or time.time() - PLATE_CLEARING_FREQ > LAST_PLATE_CLEARING:
+            clickOnPlates()
+
+        # Check if ingredient orders have arrived.
+        updateInventory()
+
+        # Go through and see if any back orders can be filled.
+        for pos, order in copy.copy(backOrders).items():
+            result = makeOrder(order)
+            if result is None:
+                del backOrders[pos] # remove from back orders
+                logging.debug('Filled back order for %s.' % (order))
+
+
+        if random.randint(1, 5) == 1:
+            # order any ingredients that are below the minimum amount
+            for ingredient, amount in INVENTORY.items():
+                if amount < MIN_INGREDIENTS:
+                    orderIngredient(ingredient)
+
+
+        # check for the "You Win" or "You fail" messages
+        if time.time() - 12 > LAST_GAME_OVER_CHECK:
+            result = checkForGameover()
+            if result == LEVEL_WIN_MESSAGE:
+                # player has completed the level
+
+                # Reset inventory and orders.
+                INVENTORY = {SHRIMP: 5, RICE: 10,
+                             NORI: 10, ROE: 10,
+                             SALMON: 5, UNAGI: 5}
+                ORDERING_COMPLETE = {SHRIMP: None, RICE: None, NORI: None,
+                                     ROE: None, SALMON: None, UNAGI: None}
+                backOrders = {}
+                remakeOrders = {}
+                currentOrders = {}
+                oldOrders = {}
+
+                logging.debug('Level %s complete.' % (LEVEL))
+                LEVEL += 1
+                time.sleep(5) # give another 5 seconds to tally score
+
+                # Click buttons to continue to next level.
+                pos = pyautogui.locateCenterOnScreen(imPath('continue_button.png'), region=GAME_REGION)
+                pyautogui.click(pos, duration=0.25)
+                logging.debug('Clicked on Continue button.')
+                pos = pyautogui.locateCenterOnScreen(imPath('continue_button.png'), region=GAME_REGION)
+
+                if LEVEL <= 7: # click the second continue if the game isn't finished.
+                    pyautogui.click(pos, duration=0.25)
+                    logging.debug('Clicked on Continue button.')
+
+                oldOrders = currentOrders
+
+
+def clickOnPlates():
+    """Clicks the mouse on the six places where finished plates will be flashing. This function does
+    not check for flashing plates, but simply clicks on all six places """
+
+    # Sets LAST_PLATE_CLEARING to the current time
+    global LAST_PLATE_CLEARING
+
+    # just blindly click on all 6 spot where a plates should be
+    for i in range(6):
+        pyautogui.click(83 + GAME_REGION[0] + (i * 101), GAME_REGION[1] + 203)
+    LAST_PLATE_CLEARING = time.time()
+
+
+def getOrders():
+    """Scans the screen for orders being made. Returns a dictionary with a (left, top, width, height)
+    tuple of integers for keys and the order constant for a value."""
+
+    # The order constants are ONIGIRI, GUNKAN_MAKI, CALIFORNIA_ROLL, SALMON_ROLL, SHRIMP_SUSHI, UNAGI_ROLL,
+    # DRAGON_ROLL, COMBO.
+    orders = {}
+    for orderType in (ALL_ORDER_TYPES):
+        allOrders = pyautogui.locateAllOnScreen(imPath('%s_oder.png' % orderType), region=(GAME_REGION[0] + 32
+                                                                                           GAME_REGION[1] + 46, 558, 44)
+        for order in allOrders:
+            orders[order] = orderType
+        return orders
+
+def getOrdersDifference(newOrders, oldOrders):
+    """Finds the differences between the orders dictionaries passed. Return value is a tuple of two dictionaries.
+    The first dictionary is the "added" dictionaty of orders newOrders <- oldOrders.
+    The second dictionary is the "removed" dictionary of orders in oldOrders but removed in newOrders.
+    Each dictionary has (left, top, width, height) for keys and an order constant for a value."""
+    added = {}
+    removed = {}
+
+    # find all orders in newOrders that are new and not found in oldOrders
+    for k in newOrders:
+        if k not in oldOrders:
+            added[k] = newOrders[k]
+    # find all orders in oldOrders that were removed and not found in newOrders
+    for k in oldOrders:
+        if k not in newOrders:
+            removed[k] = oldOrders[k]
+
+    return added, removed
